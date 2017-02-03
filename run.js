b5eb0491f7462f3c63e8776b64cd5349ffa340ab
quantumtom <cornyn@gmail.com>
2017-02-03 10:53:41 -0800

    make it better

--- run.js
+++ run.js
@@ -12,15 +12,27 @@ app.use('/', express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-    console.log('listening');}
+app.listen(app.get('port'), function () {
+        console.log('listening');
+    }
 );
 
-var multer  = require('multer');
-var upload = multer({ dest: 'dist/media/' });
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log(file);
+        cb(null, 'dist/media');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now());
+    }
+});
+
+var upload = multer({storage: storage});
 
 app.post('/uploads', upload.single('sourceVideo'), function (req, res, next) {
-    console.log(req.body);
-    console.log(req.file);
+    //console.log(req.body);
+    //console.log(req.file);
     res.send(req.file);
 });
