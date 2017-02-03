9e721a237047cfe2bd11489555473656f5d4810c
quantumtom <cornyn@gmail.com>
2017-02-03 11:40:21 -0800

    Remove more build tools.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,75 +33,8 @@
                         'media/'
                     ],
                     dest: 'dist'
-                },
-                all: {
-                    expand: true,
-                    src: [
-                        '*.html',
-                        'img/**',
-                        'css/**',
-                        'demo/**',
-                        'fonts/**',
-                        'js/**',
-                        'media/**/*'
-                    ],
-                    dest: 'dist'
-                },
-                /**
-                 * Copies original source from src/js to build/js/src/js for source map debugging.
-                 */
-                src: {
-                    files: [
-                        {
-                            expand: true,
-                            src: '*.js',
-                            dest: 'dist/js/src/js'
-                        }
-                    ]
-                },
-                other: {
-                    files: [
-                        {
-                            expand: true,
-                            src: [
-                                'img/**',
-                                'cache.manifest',
-                                'favicon.ico'
-                            ],
-                            dest: 'dist/'
-                        }
-                    ]
-                },
-                js: {
-                    files: [
-                        {
-                            expand: true,
-                            src: '**',
-                            dest: 'dist/js'
-                        }
-                    ]
                 }
-            },
-            htmlmin: {
-                build: {
-                    options: {
-                        removeComments: true,
-                        collapseWhitespace: true
-                    },
-                    files: {
-                        'dist/index.html': 'index.html'
-                    }
-                }
-            },
-            jshint: {
-                options: {
-                    reporter: require('jshint-stylish'),
-                    jshintrc: true
-                },
-                test: [
-                    'js/app/**/*.js',
-                    'Gruntfile.js'
-                ]
+
             },
             bump: {
                 options: {
@@ -119,14 +52,6 @@
                     regExp: false
                 }
             },
-            connect: {
-                server: {
-                    options: {
-                        port: 8000,
-                        base: 'dist'
-                    }
-                }
-            },
             clean: {
                 dist: {
                     src: [
@@ -135,13 +60,10 @@
                 }
             },
             watch: {
-                options: {
-                    atBegin: true
-                },
                 build: {
                     files: watchedFiles,
                     tasks: [
-                        'default'
+                        'build'
                     ]
                 }
             }
@@ -149,11 +71,7 @@
 
         grunt.loadNpmTasks('grunt-contrib-clean');
         grunt.loadNpmTasks('grunt-contrib-copy');
-        grunt.loadNpmTasks('grunt-contrib-cssmin');
-        grunt.loadNpmTasks('grunt-contrib-htmlmin');
         grunt.loadNpmTasks('grunt-contrib-watch');
-        grunt.loadNpmTasks('grunt-contrib-connect');
-        grunt.loadNpmTasks('grunt-shell');
         grunt.loadNpmTasks('grunt-bump');
 
         /**
@@ -164,12 +82,7 @@
 
         grunt.registerTask('build', [
             'clean',
-            'copy:build',
-            'htmlmin:build'
-        ]);
-
-        grunt.registerTask('test', [
-            'jshint'
+            'copy'
         ]);
 
     };
